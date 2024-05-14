c4124f9cdaa5331b71ef028ec379da7ee4ea95dd
GeorgeYL <31640708+GeorgeYL@users.noreply.github.com>
2024-05-14 17:03:09 +0800

    Create ByteEditor.py

---
+++ ByteEditor.py
@@ -0,0 +1,87 @@
+# file: ByteEditor.py: 
+# Description: set the bit sequence for a specified Byte.
+# eg: Byte1 as 8 bits:
+# 小头序: bit7 bit6 ..... bit1, bit0:  11001111 = 0XCF
+# So the input <3.3> will set byte3: 1111 1000 = 0XF8 
+# eg: <5.4> will set byte5: 1111 0000 = 0xF0
+
+import tkinter as tk
+from tkinter import messagebox
+
+class myByte:
+    def __init__(self,init_window_name):
+        self.init_window =init_window_name
+        self.entry_bits = [[tk.StringVar(value='0') for _ in range(8)] for _ in range(8)]     
+        self.init_input = tk.StringVar()
+        
+    # set the window
+    def set_init_bytes(self):
+        self.bytes_count = 8
+        
+    def update_bit_value(self, *args):
+        text = self.init_input.get()
+        if text.find(".") < 0 or len(text) > 3:
+            messagebox.showerror("error","Please input the init value as <m.n>",)
+            self.init_input.set("")
+        byteSeq = text[0]
+        bitSeq = text[2]
+        print (f"will set Byte {byteSeq}'s value to 1 after bit {bitSeq}")
+        
+        for i in range(8-int(bitSeq)):
+            self.entry_bits[int(byteSeq)-1][i].set(1)            
+        self.update_hex(byteSeq)
+    
+    def set_init_window(self):    
+        # Create entry widgets for bits 
+        # init the 2Dimension array for saving Bytes[8][8]
+        self.entry_bits = [[tk.StringVar(value='0') for _ in range(8)] for _ in range(8)]         
+        self.list_hex = [tk.StringVar(value='0') for _ in range(8)]    
+        
+        self.tip_label = tk.Label(self.init_window, text="Input:<m.n>", width=10,font =15)
+        self.tip_label.grid(row=1, column=0, padx=7, pady=7)
+        
+        self.setentry = tk.Entry(self.init_window, width=10,textvariable = self.init_input, bg="pink",  font=25)
+        self.setentry.grid(row=1, column=1, padx=7, pady=7) 
+        self.setentry.bind("<Return>", self.update_bit_value)
+        for i in range(8): 
+            for j in range(8): # every byte has 8 bits. Crated 1 text for 1 bit.
+                # Create label for hexadecimal value
+                self.tip_label = tk.Label(self.init_window, text="BYTE"+str(i+1), width=7,font =15)
+                self.tip_label.grid(row=i+3, column=1, padx=7, pady=7)
+                
+                if i%2 != 0:
+                    self.entry = tk.Entry(self.init_window, width=3, textvariable=self.entry_bits[i][j], bg = "pink",font =15)
+                else:
+                    self.entry = tk.Entry(self.init_window, width=3, textvariable=self.entry_bits[i][j],font =15)
+                
+                self.entry.grid(row=i+3, column=j+2, padx=7, pady=7)
+                self.entry.bind("<KeyRelease>", lambda event, idx=(i,j): self.update_hex(i))
+            
+                # Create label for hexadecimal value
+                if i%2 != 0:
+                    self.list_hex[i] = tk.Label(self.init_window, text="", width=15, bg="red",font =15)
+                else:
+                    self.list_hex[i] = tk.Label(self.init_window, text="", width=15,font =15)                
+                self.list_hex[i].grid(row=i+3, column=8+5, padx=15, pady=15)
+        
+    def update_hex(self, byte_row):
+        for row in range(8):
+            binary ="".join(self.entry_bits[row][i].get() for i in range(8))
+            if binary:
+                self.list_hex[row].config(text="0x" + format(int(binary.replace("\n", ""), 2), '02X'))
+            else:
+                self.list_hex[row].config(text="0x00")
+
+def gui_start():
+    init_window = tk.Tk()
+    init_window.title("!ByteEditor: For Byte m, set all of the value to 1 after the <n>th bit; eg: set 4.3==>> 11111000 (0xF8)")
+    
+    app = myByte(init_window)
+    app.set_init_bytes()
+    app.set_init_window()
+    app.init_window.mainloop()
+    
+if __name__ == '__main__':
+    gui_start()
+else:
+    print("it's not heree")
